1193042fedbc493afeb18005600c25a5692a3c9b
Manoj <seenumanoj2006@gmail.com>
2026-07-08 19:02:05 +0530

    cros

--- server/server.js
+++ server/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: true,
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173', 
+    'https://apjbilling.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
