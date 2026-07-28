5b3b40aac50679d726199374874cd5cf221aa4b0
Manoj <seenumanoj2006@gmail.com>
2026-07-28 21:33:16 +0530

    comit the correction

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:5173',
     'https://apjbilling.vercel.app',
-    'httpshttps://apjbilling-ten.vercel.app'
+    'https://apjbilling-ten.vercel.app'
   ],
   credentials: true
 }));
